1f436cf52d2a00d4224c531a0c8095e5a5f7d489
Hansel Wei <darkmastermindz@gmail.com>
2017-08-10 04:42:35 -0400

    * Added bot functionallity * Updated node modules

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-//Entry point
-
+// Load the discordbot-framework module
+const bot_name = "Darkus";
 const DiscordBot = require('discordbot-framework');
 let bot = new DiscordBot();
+
+const configData = {
+  'secret_key'  : '',
+  'command_prefix'  : '?',
+  'respond_to_bots'  : 'false',
+  'secret_key'  : 'n33CPE1N5OS2tjwgptEbWfPcHwWkonK7',
+  'boot_msg'  : 'Hi! I\'m" + bot_name + "Nice to meet you.'
+};
+
+
+// Add a listener
+bot.observe('guildMemberAdd', (guildMember) => {
+    const nickname = guildMember.nickname || guildMember.user.username;
+    guildMember.guild.defaultChannel.sendMessage(`Welcome to the ${guildMember.guild.name} party, ${nickname}!`);
+});
---
+++ stream.js
@@ -0,0 +1,2 @@
+//Used to pull audio data stream
+//Plan to do Soundcloud and Youtube.
